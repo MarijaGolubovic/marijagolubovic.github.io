827ffae0fe6822a0965a1cf1751ed0e8825745ce
Marija Golubovic <marijagolubovic16@gmail.com>
2025-01-23 23:58:38 +0100

    fix offset for small device

--- script.js
+++ script.js
@@ -89,10 +89,10 @@ function moveCards(direction) {
     let moveBy;
     if (window.innerWidth <= 768) {
         if (direction === 'right') {
-        moveBy = -currentIndex * (cards[0].offsetWidth) - 30; // Calculate how much to move
+        moveBy = -currentIndex * (cards[0].offsetWidth) - 15*currentIndex; // Calculate how much to move
         }
         else{
-        moveBy = -currentIndex * (cards[0].offsetWidth); // Calculate how much to move
+        moveBy = -currentIndex * (cards[0].offsetWidth) + 15*currentIndex; // Calculate how much to move
 
         }
     }
